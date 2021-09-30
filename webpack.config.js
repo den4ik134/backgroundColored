170a9a481a094d3ab3ca227441b306b411376cfb
Denys <bilko.dp@gmail.com>
2021-09-30 20:05:38 +0300

    redy to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'production',
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './root'),
         filename: 'index.js',
         clean: true
     },
